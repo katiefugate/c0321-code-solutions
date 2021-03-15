fafb0dc413b9900aab6947bf305fd5bd2b55f77b
katiefugate <katiiierochelle@yahoo.com>
2021-03-15 12:50:58 -0700

    completed javascript-operators-and-expressions. katiefugate

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,43 @@
+var width = 5;
+var height = 5;
+
+var area = width + height;
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 5;
+var payment = 10;
+
+var change = payment - bill;
+console.log('value of change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 10;
+var midterm = 15;
+var final = 20;
+
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Katie';
+var lastName = 'Fugate';
+
+var fullName = firstName + ' ' + lastName;
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var ph = 5;
+var isAcidic = ph < 7;
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+var headCount = 25;
+var isSparta = headCount === 300;
+console.log('value of isSparta', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+var motto = fullName;
+motto += ' is the GOAT';
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
