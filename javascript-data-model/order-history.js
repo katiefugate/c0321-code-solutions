0cdf8036158455843cbb4a295a7d7896e62a1feb
katiefugate <katiiierochelle@yahoo.com>
2021-03-16 12:39:51 -0700

    made requested changes.

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,72 +1,74 @@
 var orderHistory = [
   {
     placed: 'August 4, 2020',
-    total: '$34.00',
+    total: 34.00,
     shipTo: 'JS Masher',
     orderNum: 'ORDER # 114-3941689-8772232',
-    delivered: 'Delivered Aug 8, 2020',
+    delivered: 'Aug 8, 2020',
+    packageLeft: null,
     items: [
       {
         item: 'JavaScript for impatient programmers',
         img: '',
         author: 'Rauschmayer, Dr. Axel',
-        price: '$31.55',
-        returnWindow: 'Return window closed on Sep 7, 2020'
+        price: 31.55,
+        returnWindow: 'Sep 7, 2020'
       }
     ]
   },
   {
     placed: 'July 19, 2020',
-    total: '$44.53',
+    total: 44.53,
     shipTo: 'JS Masher',
     orderNum: 'ORDER # 113-9984268-1280257',
-    delivered: 'Delivered Jul 20, 2020',
+    delivered: 'Jul 20, 2020',
     packageLeft: 'Your package was delivered. It was handed directly to a resident',
     items: [
       {
         item: 'The Timeles Way of Building',
         img: '',
         author: 'Alexander, Chrisopher',
-        price: '$41.33',
-        returnWindow: 'Return window closed on Aug 19, 2020'
+        price: 41.33,
+        returnWindow: 'Aug 19, 2020'
       }
     ]
   },
   {
     placed: 'July 4, 2020',
-    total: '$17.22',
+    total: 17.22,
     shipTo: 'JS Masher',
     orderNum: 'ORDER # 114-2875557-9059409',
-    delivered: 'Delivered Jul 7, 2020',
+    delivered: 'Jul 7, 2020',
     packageLeft: 'Your package was delivered. It was handed directly to a resident.',
     items: [
       {
         item: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
         img: '',
-        price: '$15.98',
-        returnWindow: 'Return window closed on Aug 5, 2020'
+        price: 15.98,
+        returnWindow: 'Aug 5, 2020'
       }
     ]
   },
   {
     placed: 'July 3, 2020',
-    total: '$138.93',
+    total: 138.93,
     shipTo: 'JS Masher',
     orderNum: 'ORDER # 113-2883177-2648248',
-    delivered: 'Delivered Jul 5, 2020',
+    delivered: 'Jul 5, 2020',
+    packageLeft: null,
     items: [
       {
         item: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
         img: '',
-        price: '$94.95',
-        returnWindow: 'Return window closed on Aug 4, 2020'
+        price: 94.95,
+        returnWindow: 'Aug 4, 2020'
       },
       {
         item: 'The Art of Sql',
         img: '',
         author: 'Faroult, Stephanie',
-        price: '$33.99',
-        returnWindow: 'Return window closed on Aug 4, 2020'
+        price: 33.99,
+        returnWindow: 'Aug 4, 2020'
       }
     ]
   }
