9214d1797605fbb918ab6ff25ea37844ea1319bc
katiefugate <katiiierochelle@yahoo.com>
2021-03-22 11:58:40 -0700

    completed dom-querying.

--- array-manipulation-2/src/includes.js
+++
@@ -1,9 +0,0 @@
-/* exported includes */
-function includes(array, value) {
-  for (var i = 0; i < array.length; i++) {
-    if (array[i] === value) {
-      return true;
-    }
-  }
-  return false;
-}
