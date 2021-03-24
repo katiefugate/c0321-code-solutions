499714af6746cb821720e9758b4aad14926984d2
katiefugate <katiiierochelle@yahoo.com>
2021-03-24 10:33:02 -0700

    in progress.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,12 +1,54 @@
+// var parent = document.querySelector('.container')
 // var letters = document.querySelectorAll('span');
-// var lettersCount = 0;
 
-// function typing(event) {
-//   for(var i = 0; i < letters.length; i++)
-//   if(document.onkeydown === letters[i]) {
-//     letters[i].className = 'correct';
-//   }
-//   else {
-//     letters[i].className = 'incorrect';
+// function idk() {
+//   var i = 0;
+//   while (i < letters.length) {
+//     var currentLetter = letters[i].textContent;
+//     if (currentLetter === eachLetter()){
+//     }
+//     if (currentLetter.className === 'correct') {
+//       i++
+//     }
 //   }
 // }
+
+// function eachLetter(currentValue) {
+//   var typedLetter = currentValue.key;
+//   console.log(typedLetter);
+//   return typedLetter;
+//   }
+// document.addEventListener('keydown', eachLetter);
+
+// letters.forEach(eachLetter);
+
+// document.addEventListener('keydown', eachLetter);
+
+// function typedLetter(event) {
+//   for(i = 0; i < letters.length; i++){
+//   var currentLetter = ;
+//   }
+//   console.log(currentLetter)
+//   return currentLetter;
+// }
+
+// document.addEventListener('keydown', currentLetterF)
+// function eachLetter(currentValue) {
+
+//   if (currentValue === currentLetterF) {
+//     currentValue.className = 'correct';
+//     // console.log(currentLetter)
+//   }
+// }
+// letters.forEach(eachLetter);
+
+// function eachLetter(currentValue) {
+//   // for (i = 0; i < letters.length; i++){
+
+//     if(currentValue.textContent === currentLetter ) {
+//       currentValue.className = 'correct';
+//     }
+//   // }
+// }
+// // document.addEventListener('keydown', letterValue)
+// letters.forEach(eachLetter);
