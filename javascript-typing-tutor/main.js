bbbd74d3b6f021b90b1cd95aeb0f9cb20beb0c0e
katiefugate <katiiierochelle@yahoo.com>
2021-03-23 18:05:16 -0700

    in progress.

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,12 @@
+// var letters = document.querySelectorAll('span');
+// var lettersCount = 0;
+
+// function typing(event) {
+//   for(var i = 0; i < letters.length; i++)
+//   if(document.onkeydown === letters[i]) {
+//     letters[i].className = 'correct';
+//   }
+//   else {
+//     letters[i].className = 'incorrect';
+//   }
+// }
