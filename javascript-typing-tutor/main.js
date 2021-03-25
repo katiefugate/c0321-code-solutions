699b6771f223fd19b5b62e45d7cbe66a853f21a7
katiefugate <katiiierochelle@yahoo.com>
2021-03-24 20:59:00 -0700

    in progress.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -4,62 +4,24 @@
 // var typedLetter;
 // function eachLetter(currentValue) {
 //   typedLetter = currentValue.key;
-//   // console.log(typedLetter);
+//   console.log('space', currentValue);
 //   idk(typedLetter);
 // }
 // var currentLetter;
-
-// function idk(currentValue) {
-//   var i = 0;
-//   // console.log(currentValue);
-//   // console.log(letters[0].className)
-//   while (i < letters.length - 1) {
-//     currentLetter = letters[i].textContent;
-//     if (currentLetter === currentValue){
-//       letters[i].className = 'correct';
-//       console.log(letters[i].className)
-//     }
-//     else if (letters[i].className === 'correct') {
-//       i++
-//     }
-//     else if (currentLetter !== currentValue) {
-//       letters[i].className = 'incorrect';
-//     }
+// var t = 0;
+// function idk(letter) {
+//   console.log('letter', letter)
+//   console.log('letters[t]', letters[t])
+//   if(letter === '&nbsp;') {
+//     letter = ' ';
 //   }
-
+//   if(letter === letters[t].textContent) {
+//     letters[t].className = 'correct';
+//     t++;
+//   }
+//     else {
+//       letters[t].className = 'incorrect';
+//     }
 // }
 
 // document.addEventListener('keydown', eachLetter);
-
-// letters.forEach(eachLetter);
-
-// document.addEventListener('keydown', eachLetter);
-
-// function typedLetter(event) {
-//   for(i = 0; i < letters.length; i++){
-//   var currentLetter = ;
-//   }
-//   console.log(currentLetter)
-//   return currentLetter;
-// }
-
-// document.addEventListener('keydown', currentLetterF)
-// function eachLetter(currentValue) {
-
-//   if (currentValue === currentLetterF) {
-//     currentValue.className = 'correct';
-//     // console.log(currentLetter)
-//   }
-// }
-// letters.forEach(eachLetter);
-
-// function eachLetter(currentValue) {
-//   // for (i = 0; i < letters.length; i++){
-
-//     if(currentValue.textContent === currentLetter ) {
-//       currentValue.className = 'correct';
-//     }
-//   // }
-// }
-// // document.addEventListener('keydown', letterValue)
-// letters.forEach(eachLetter);
