0ecd6daf7152e75f544c97ebf255175c6cf987e2
katiefugate <katiiierochelle@yahoo.com>
2021-03-25 16:51:27 -0700

    completed javascript-view-swapping. katiefugate.

---
+++ javascript-view-swapping/main.js
@@ -0,0 +1,30 @@
+var tabContainer = document.querySelector('.tab-container');
+var tabs = document.querySelectorAll('.tab');
+var views = document.querySelectorAll('.view');
+
+function clickHandler(event) {
+  if (event.target.matches('.tab')) {
+    for (var i = 0; i < tabs.length; i++) {
+      if (tabs[i] === event.target) {
+        tabs[i].className = 'tab active';
+      } else {
+        tabs[i].className = 'tab';
+      }
+    }
+  }
+  if (event.target.className !== 'tab active') {
+    return;
+  }
+
+  var view = event.target.dataset.view;
+
+  for (var k = 0; k < views.length; k++) {
+    if (views[k].getAttribute('data-view') !== view) {
+      views[k].className = 'view hidden';
+    } else {
+      views[k].className = 'view';
+    }
+  }
+}
+
+tabContainer.addEventListener('click', clickHandler);
