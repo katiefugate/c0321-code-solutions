63245ddad52aa31cd5c4aa3c61770cd36a3cfdfe
katiefugate <katiiierochelle@yahoo.com>
2021-03-26 15:19:06 -0700

    completed javascript-typing-tutor. katiefugate.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,54 +1,56 @@
-// var parent = document.querySelector('.container')
-// var letters = document.querySelectorAll('span');
-// var typedLetter;
-// var typeCount = 0;
-// function eachLetter(currentValue) {
-//   typedLetter = currentValue.key;
-//   typeThis(typedLetter);
-//   typeCount++
-//   percentage();
-// }
+var letters = document.querySelectorAll('span');
+var typedLetter;
+var typeCount = 0;
+function eachLetter(currentValue) {
+  typedLetter = currentValue.key;
+  typeThis(typedLetter);
+  typeCount++;
+  percentage();
+}
 
-// var t = 0;
-// var incorrect = 0;
-// letters[0].className = 'current'
-// function typeThis(letter) {
-//   if(letter === letters[t].textContent) {
-//     letters[t].className = 'correct';
-//     letters[t + 1].className = 'current';
-//     t++;
-//   }
-//   else {
-//     letters[t].className = 'incorrect';
-//       incorrect++
-//     }
-//     if(t === letters.length - 1) {
-//       console.log('end')
-//       return;
-//     }
-// }
-// function percentage() {
-//   if (t === letters.length - 1) {
-//     var correct = typeCount - incorrect;
-//     var percent = (correct / typeCount) * 100;
-//     percent = Math.round(percent);
-//     var popUpContainer = document.querySelector('.popUpContainer');
-//     var popUp = document.createElement('div');
-//     var percentCorrect = document.createElement('p');
-//     var again = document.createElement('p');
-//     var yesButton = document.createElement('button');
-//     popUpContainer.appendChild(popUp);
-//     popUp.appendChild(percentCorrect);
-//     popUp.appendChild(again);
-//     popUp.appendChild(yesButton);
-//     percentCorrect.textContent = 'Your typing accuracy is ' + percent + '%!';
-//     again.textContent = 'Would you like to play again?';
-//     yesButton.textContent = 'YES!';
-//     percentCorrect.className = 'percent';
-//     popUp.className = 'popUp';
-//     percentCorrect.className = 'percent';
-//     again.className = 'again';
-//   }
-// }
+var t = 0;
+var incorrect = 0;
+letters[0].className = 'current';
+function typeThis(letter) {
+  if (letter === letters[t].textContent) {
+    letters[t].className = 'correct';
+    letters[t + 1].className = 'current';
+    t++;
+  } else {
+    letters[t].className = 'incorrect';
+    incorrect++;
+  }
+}
+function percentage() {
+  if (t === letters.length - 1) {
+    var correct = typeCount - incorrect;
+    var percent = (correct / typeCount) * 100;
+    percent = Math.round(percent);
+    var popUpContainer = document.createElement('div');
+    var popUp = document.createElement('div');
+    var percentCorrect = document.createElement('p');
+    var again = document.createElement('p');
+    var yesButton = document.createElement('button');
+    var body = document.querySelector('body');
+    body.appendChild(popUpContainer);
+    popUpContainer.appendChild(popUp);
+    popUp.appendChild(percentCorrect);
+    popUp.appendChild(again);
+    popUp.appendChild(yesButton);
+    percentCorrect.textContent = 'Your typing accuracy is ' + percent + '%!';
+    again.textContent = 'Would you like to play again?';
+    yesButton.textContent = 'YES!';
+    popUpContainer.className = 'popUpContainer';
+    percentCorrect.className = 'percent';
+    popUp.className = 'popUp';
+    percentCorrect.className = 'percent';
+    again.className = 'again';
+    yesButton.addEventListener('click', clickHandler);
+  }
+}
 
-// document.addEventListener('keydown', eachLetter);
+function clickHandler(event) {
+  location.reload();
+}
+
+document.addEventListener('keydown', eachLetter);
