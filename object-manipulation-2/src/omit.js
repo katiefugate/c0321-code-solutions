a753739d2e624dcb8d7311399d78b178d40432f2
katiefugate <katiiierochelle@yahoo.com>
2021-04-05 10:07:25 -0700

    changes in progress.

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,13 +1,12 @@
 /* exported omit */
 
-function omit(source, keys) {
-  var copy = Object.assign({}, source);
-  for (var k = 0; k < keys.length; k++) {
-    for (var key in copy) {
-      if (key === keys[k]) {
-        delete copy[key];
-      }
-    }
-  }
-  return copy;
-}
+// function omit(source, keys) {
+//   var copy = Object.assign({}, source);
+
+//   for (var k = 0; k < keys.length; k++) {
+//       if (key in copy === keys[k]) {
+//         delete copy[keys[k]];
+//       }
+//   }
+//   return copy;
+// }
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,16 +1,15 @@
 /* exported pick */
 
-function pick(source, keys) {
-  var newObj = {};
-
-  for (var i = 0; i < keys.length; i++) {
-    for (var key in source) {
-      if (source[key] === undefined) {
-        source[key] = undefined;
-      } else if (key === keys[i]) {
-        newObj[key] = source[key];
-      }
-    }
-  }
-  return newObj;
-}
+// function pick(source, keys) {
+//   var copy = Object.assign({}, source);
+//   var newObj = {};
+//   for (var i = 0; i < keys.length; i++) {
+//     if(copy[keys[i]] === undefined) {
+//       copy[keys[i]] = undefined;
+//     }
+//     else if (copy.hasOwnProperty(keys[i])) {
+//       newObj[keys[i]] = copy[keys[i]];
+//     }
+//   }
+//   return newObj;
+// }
