2985eb17fd0fc92356b902339fae12d1d9ebe512
katiefugate <katiiierochelle@yahoo.com>
2021-04-05 14:42:43 -0700

    made changes.

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -3,9 +3,7 @@
 function pick(source, keys) {
   var newObj = {};
   for (var i = 0; i < keys.length; i++) {
-    if (source[keys[i]] === undefined) {
-      source[keys[i]] = undefined;
-    } else if (keys[i] in source) {
+    if (keys[i] in source && source[keys[i]] !== undefined) {
       newObj[keys[i]] = source[keys[i]];
     }
   }
