b5bd4bfa878453bde7fd9bb08c4610f825f38261
katiefugate <katiiierochelle@yahoo.com>
2021-04-06 09:59:12 -0700

    in progress.

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,1 +1,14 @@
 /* exported isAnagram */
+
+function isAnagram(firstString, secondString) {
+  var firstNoSpace = firstString.replaceAll(' ', '');
+  var secondNoSpace = secondString.replaceAll(' ', '');
+  var counter = 0;
+
+  for (var i = 0; i < secondNoSpace.length; i++) {
+    if (firstNoSpace.includes(secondNoSpace[i])) {
+      counter++;
+    }
+  }
+  return counter === firstNoSpace.length;
+}
--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,13 @@
 /* exported reverseWords */
+
+// function reverseWords(string) {
+//   var strArr = string.split('');
+//   var reverseArr = []
+//   for (i = 0; i < strArr.length; i++) {
+//     reverseArr.unshift(strArr[i]);
+//   }
+//   var reverseStr = reverseArr.join('');
+//   console.log(reverseArr);
+//   console.log(reverseStr);
+//   return reverseStr;
+// }
