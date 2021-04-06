13cbb00bcc9dc62219a6f0131e71f6e364106e3d
katiefugate <katiiierochelle@yahoo.com>
2021-04-06 14:56:10 -0700

    in progress.

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,13 +1,20 @@
 /* exported reverseWords */
 
-// function reverseWords(string) {
-//   var strArr = string.split('');
-//   var reverseArr = []
-//   for (i = 0; i < strArr.length; i++) {
-//     reverseArr.unshift(strArr[i]);
-//   }
-//   var reverseStr = reverseArr.join('');
-//   console.log(reverseArr);
-//   console.log(reverseStr);
-//   return reverseStr;
-// }
+function reverseWords(string) {
+  var strArr = string.split(' ');
+  var tempReverseArr = [];
+  for (var k = 0; k < strArr.length; k++) {
+    var temp = [];
+    for (var i = 0; i < strArr[k].length; i++) {
+      temp.unshift(strArr[k][i]);
+    }
+    tempReverseArr.push(temp);
+  }
+  var reverseArr = [];
+  for (var m = 0; m < tempReverseArr.length; m++) {
+    var newStr = tempReverseArr[m].join('');
+    reverseArr.push(newStr);
+    var reverse = reverseArr.join(' ');
+  }
+  return reverse;
+}
--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,25 @@
 /* exported titleCase */
+
+// function titleCase(string) {
+//   lower = string.toLowerCase();
+//   strArr = lower.split(' ');
+//   for(var i = 0; i < strArr.length; i++) {
+//     if (strArr[i] === 'javascript') {
+//       strArr[i] = 'JavaScript';
+//     }
+//     else if (strArr[i] === 'api') {
+//       strArr[i] = 'API'
+//     }
+//     else if(strArr[i].length < 4) {
+//       // strArr[i].charAt(0).toLowerCase();
+//     }
+//     else {
+//       var firstUp = strArr[i].charAt(0).toUpperCase();
+//       var end = strArr[i].slice(1, strArr[i].length);
+//       var finished = firstUp + end;
+//     }
+//     console.log(strArr[i].charAt(0))
+//   }
+//   console.log(strArr);
+//   console.log(finished)
+// }
