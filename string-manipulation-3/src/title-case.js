d31348276a104e0a51fd79c5d2764558e5321244
katiefugate <katiiierochelle@yahoo.com>
2021-04-07 09:46:07 -0700

    in progress.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -10,16 +10,41 @@
 //     else if (strArr[i] === 'api') {
 //       strArr[i] = 'API'
 //     }
-//     else if(strArr[i].length < 4) {
-//       // strArr[i].charAt(0).toLowerCase();
-//     }
-//     else {
+//     else if (strArr[i] === 'web') {
 //       var firstUp = strArr[i].charAt(0).toUpperCase();
 //       var end = strArr[i].slice(1, strArr[i].length);
 //       var finished = firstUp + end;
+//       strArr.splice(i, 1, finished)
 //     }
-//     console.log(strArr[i].charAt(0))
+//     else if (strArr[i].charAt(strArr[i].length - 1) === ':') {
+//       var firstUpp = strArr[i + 1].charAt(0).toUpperCase();
+//       var endd = strArr[i + 1].slice(1, strArr[i + 1].length);
+//       var finishedd = firstUpp + endd;
+//       strArr.splice(i + 1, 1, finishedd)
+//       if (strArr[i] === 'javascript:') {
+//         strArr[i] = 'JavaScript:'
+//       }
+//     }
+//     else if (strArr[i].includes('-')) {
+//       var splitDash = strArr[i].split('-');
+//       var firstUpper = splitDash[0].charAt(0).toUpperCase();
+//       var secondUpper = splitDash[1].charAt(0).toUpperCase();
+//       var firstEnd = splitDash[0].slice(1, splitDash[0].length);
+//       var secondEnd = splitDash[1].slice(1, splitDash[1].length);
+//       var firstFinished = firstUpper + firstEnd;
+//       var secondFinished = secondUpper + secondEnd;
+//       splitDash[0] = firstFinished;
+//       splitDash[1] = secondFinished;
+//       strArr[i] = splitDash.join('-')
+//     }
+//     else if (strArr[i].length > 3) {
+//       var firstUp = strArr[i].charAt(0).toUpperCase();
+//       var end = strArr[i].slice(1, strArr[i].length);
+//       var finished = firstUp + end;
+//       strArr.splice(i, 1, finished)
+//     }
 //   }
-//   console.log(strArr);
-//   console.log(finished)
+
+//   titleCaseString = strArr.join(' ')
+//   return titleCaseString;
 // }
