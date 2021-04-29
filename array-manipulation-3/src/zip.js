f987b671e09d1322d5ed043270b1628a2565805b
katiefugate <katiiierochelle@yahoo.com>
2021-04-29 22:31:28 +0000

    made requested changes.

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -3,20 +3,13 @@
 function zip(first, second) {
   var zipped = [];
   var temp;
-  if (second.length <= first.length) {
-    for (var i = 0; i < second.length; i++) {
-      temp = [];
-      temp.push(first[i]);
-      temp.push(second[i]);
-      zipped.push(temp);
-    }
-  } else {
-    for (var k = 0; k < first.length; k++) {
-      temp = [];
-      temp.push(first[k]);
-      temp.push(second[k]);
-      zipped.push(temp);
-    }
+  var length = Math.min(first.length, second.length);
+
+  for (var i = 0; i < length; i++) {
+    temp = [];
+    temp.push(first[i]);
+    temp.push(second[i]);
+    zipped.push(temp);
   }
   return zipped;
 }
