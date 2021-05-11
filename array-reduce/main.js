26f68f06b099c4d9102ed59486b0acfb9991d898
katiefugate <katiiierochelle@yahoo.com>
2021-05-11 22:19:21 +0000

    completed array-reduce. katiefugate.

---
+++ array-reduce/main.js
@@ -0,0 +1,38 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const account = [
+  { type: 'deposit', amount: 150 },
+  { type: 'deposit', amount: 20 },
+  { type: 'withdrawal', amount: 5 },
+  { type: 'deposit', amount: 100 },
+  { type: 'withdrawal', amount: 25 },
+  { type: 'withdrawal', amount: 60 }
+];
+
+const traits = [
+  { color: 'yellow' },
+  { type: 'electric' },
+  { name: 'pikachu' },
+  { level: 15 },
+  { trainer: 'ash' }
+];
+
+const sum = numbers.reduce((currentSum, currentNumber) => currentSum + currentNumber);
+console.log(sum);
+
+const product = numbers.reduce((currentProduct, currentNumber) => currentProduct * currentNumber);
+console.log(product);
+
+const balance = account.reduce((currentBalance, currentAccount) => {
+  if (currentAccount.type === 'withdrawal') {
+    return currentBalance - currentAccount.amount;
+  } else {
+    return currentBalance + currentAccount.amount;
+  }
+}, 0);
+console.log(balance);
+
+const composite = traits.reduce((currentComposite, currentTrait) => {
+  return Object.assign(currentComposite, currentTrait);
+});
+console.log(composite);
