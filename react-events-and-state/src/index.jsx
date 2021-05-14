bd4b4a6f5dd6c9d3e0ec42794aec8daf837a0c0c
katiefugate <katiiierochelle@yahoo.com>
2021-05-14 16:18:02 +0000

    completed react-events-and-state. katiefugate.

---
+++ react-events-and-state/src/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class CustomButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = { isClicked: false };
+  }
+
+  handleClick() {
+    this.setState({ isClicked: true });
+  }
+
+  render() {
+    if (this.state.isClicked) {
+      return <button>Thanks!</button>;
+    } else {
+      return <button onClick={this.handleClick}>Click Me!</button>;
+    }
+  }
+}
+
+const $root = document.querySelector('#root');
+
+ReactDOM.render(< CustomButton />, $root);
