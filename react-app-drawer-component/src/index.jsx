28519ca65e4ed16e57be44e03ce9598c95d8baad
katiefugate <katiiierochelle@yahoo.com>
2021-05-17 22:18:29 +0000

    completed react-app-drawer-component. katiefugate.

---
+++ react-app-drawer-component/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppDrawer from './app-drawer';
+
+const $root = document.querySelector('#root');
+ReactDOM.render(< AppDrawer />, $root);
