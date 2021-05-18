e853be29d6196f560e1484eb4ea704e671422565
katiefugate <katiiierochelle@yahoo.com>
2021-05-18 17:34:04 +0000

    in progress.

---
+++ react-accordion-component/src/accordion.jsx
@@ -0,0 +1,27 @@
+// import React from 'react'
+
+// class Accordion extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     // this.handleClick = this.handleClick.bind(this);
+//     // this.state =
+//   }
+
+//   render(props) {
+//     const topics = this.props.topics.map((eachTopic) => {
+//       return (
+//         <div key={eachTopic.topic} className={}>
+//           <h1>{eachTopic.topic}</h1>
+//           <p>{eachTopic.details}</p>
+//         </div>
+//       )
+//     })
+//     return (
+//       <div>
+//         {topics}
+//       </div>
+//       )
+//   }
+// }
+
+// export default Accordion
