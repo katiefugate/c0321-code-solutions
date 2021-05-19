1c3825338e09a66f6e1bb498f35e1169453cb5f7
katiefugate <katiiierochelle@yahoo.com>
2021-05-19 22:59:09 +0000

    in progress.

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -3,16 +3,24 @@
 // class Accordion extends React.Component {
 //   constructor(props) {
 //     super(props)
-//     // this.handleClick = this.handleClick.bind(this);
-//     // this.state =
+//     this.handleClick = this.handleClick.bind(this);
+//     this.state = {infoClass: 'info'}
+//   }
+
+//   handleClick () {
+//     if(event.target.className === 'info') {
+//       this.setState({infoClass: 'hidden'})
+//     } else {
+//       this.setState({infoClass: 'info'})
+//     }
 //   }
 
 //   render(props) {
 //     const topics = this.props.topics.map((eachTopic) => {
 //       return (
-//         <div key={eachTopic.topic} className={}>
-//           <h1>{eachTopic.topic}</h1>
-//           <p>{eachTopic.details}</p>
+//         <div key={eachTopic.topic}>
+//           <h1 onClick={this.handleClick} className="topic">{eachTopic.topic}</h1>
+//           <p className={this.state.infoClass} id={eachTopic.topic}>{eachTopic.details}</p>
 //         </div>
 //       )
 //     })
