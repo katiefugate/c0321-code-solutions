c94369e0de831c10c0f256890fc2befde5524a0c
katiefugate <katiiierochelle@yahoo.com>
2021-05-20 16:12:26 +0000

    in progress.

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,35 +1,35 @@
-// import React from 'react'
+import React from 'react';
 
-// class Accordion extends React.Component {
-//   constructor(props) {
-//     super(props)
-//     this.handleClick = this.handleClick.bind(this);
-//     this.state = {infoClass: 'info'}
-//   }
+class Accordion extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = { infoClass: 'info' };
+  }
 
-//   handleClick () {
-//     if(event.target.className === 'info') {
-//       this.setState({infoClass: 'hidden'})
-//     } else {
-//       this.setState({infoClass: 'info'})
-//     }
-//   }
+  handleClick() {
+    if (event.target.className === 'info') {
+      this.setState({ infoClass: 'hidden' });
+    } else {
+      this.setState({ infoClass: 'info' });
+    }
+  }
 
-//   render(props) {
-//     const topics = this.props.topics.map((eachTopic) => {
-//       return (
-//         <div key={eachTopic.topic}>
-//           <h1 onClick={this.handleClick} className="topic">{eachTopic.topic}</h1>
-//           <p className={this.state.infoClass} id={eachTopic.topic}>{eachTopic.details}</p>
-//         </div>
-//       )
-//     })
-//     return (
-//       <div>
-//         {topics}
-//       </div>
-//       )
-//   }
-// }
+  render(props) {
+    const topics = this.props.topics.map(eachTopic => {
+      return (
+        <div key={eachTopic.topic}>
+          <h1 onClick={this.handleClick} className="topic">{eachTopic.topic}</h1>
+          <p className={this.state.infoClass} id={eachTopic.topic}>{eachTopic.details}</p>
+        </div>
+      );
+    });
+    return (
+      <div>
+        {topics}
+      </div>
+    );
+  }
+}
 
-// export default Accordion
+export default Accordion;
--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -1,28 +1,30 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-// import Accordion from './accordion'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Accordion from './accordion';
 
-// const topics = [
-//   {
-//     topic: 'Hypertext Markup Language',
-//      details: `Hypertext Markup Language (HTML) is the standard markup language
-//      for creating web pages and web applications.With Cascading Style Sheets
-//      (CSS) and JavaScript, it forms a triad of cornerstone technologies for the
-//      World Wide Web.`
-//     },
-//   {
-//     topic: 'Cascading Style Sheets',
-//     details: `Cascading Style Sheets (CSS) is a style sheet language used for
-//         describing the presentation of a document written in a markup language
-//         like HTML. CSS is a cornerstone tehcnology of the World Wide Web
-//         alongside HTML and JavaScript.`},
-//   {
-//     topic: 'JavaScript',
-//     details: `JavaScript, often abbreviated as JS, is a high-level, interpreted
-//         programming language that conforms to the ECMAScript specification.
-//         JavaScript has curly-bracket syntax, dynamic typing, prototype-based
-//         object-orientation, and first-class functions.`}
-// ]
+const topics = [
+  {
+    topic: 'Hypertext Markup Language',
+    details: `Hypertext Markup Language (HTML) is the standard markup language
+     for creating web pages and web applications.With Cascading Style Sheets
+     (CSS) and JavaScript, it forms a triad of cornerstone technologies for the
+     World Wide Web.`
+  },
+  {
+    topic: 'Cascading Style Sheets',
+    details: `Cascading Style Sheets (CSS) is a style sheet language used for
+        describing the presentation of a document written in a markup language
+        like HTML. CSS is a cornerstone tehcnology of the World Wide Web
+        alongside HTML and JavaScript.`
+  },
+  {
+    topic: 'JavaScript',
+    details: `JavaScript, often abbreviated as JS, is a high-level, interpreted
+        programming language that conforms to the ECMAScript specification.
+        JavaScript has curly-bracket syntax, dynamic typing, prototype-based
+        object-orientation, and first-class functions.`
+  }
+];
 
-// const $root = document.querySelector('#root')
-// ReactDOM.render(< Accordion topics={topics}/>, $root)
+const $root = document.querySelector('#root');
+ReactDOM.render(< Accordion topics={topics}/>, $root);
